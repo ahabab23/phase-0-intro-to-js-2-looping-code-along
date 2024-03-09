1837147e88f8355d4e720eb489fa52d85ba9df43
Uwes Ahabab <uwesahabab@gmail.com>
2024-03-09 13:26:07 +0300

    Looping code

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Code your solutions in this file
+function writeCards(arr, birthday){
+    let ar=[]
+
+    for (let i=0;i<arr.length;i++){
+        ar.push(`Thank you, ${arr[i]}, for the wonderful ${birthday} gift!`);
+        console.log(ar)
+    
+    }
+    
+    return ar;
+
+}
+writeCards(["Charlie", "Samip", "Ali"], "birthday");
+
+function countDown(z){
+    while(z>=0){
+        console.log(z)
+        z--
+    }
+    return z
+}
+
+countDown(10)
